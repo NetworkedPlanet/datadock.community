2968928ac7684a06f217906ffbd9c1aba25a6d1a
Kal Ahmed <kal@networkedplanet.com>
2018-03-12 11:22:25 +0000

    Validate models using FluentValidation

---
+++ src/Datadock.Common/Repositories/UserAccountNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Datadock.Common.Repositories;
+
+namespace Datadock.Common.Elasticsearch
+{
+    public class UserAccountNotFoundException : UserRepositoryException
+    {
+        public UserAccountNotFoundException(string userId) : base($"Could not find account record for user {userId}")
+        {
+        }
+    }
+}
---
+++ src/Datadock.Common/Validators/JobInfoValidator.cs
@@ -0,0 +1,20 @@
+﻿using Datadock.Common.Models;
+using FluentValidation;
+
+namespace Datadock.Common.Validators
+{
+    public class JobInfoValidator : AbstractValidator<JobInfo>
+    {
+        public JobInfoValidator()
+        {
+            RuleFor(x => x.JobId).NotEmpty();
+            RuleFor(x => x.UserId).NotEmpty();
+            RuleFor(x => x.OwnerId).NotEmpty();
+            RuleFor(x => x.RepositoryId).NotEmpty();
+            RuleFor(x => x.GitRepositoryFullName).NotEmpty();
+            RuleFor(x => x.GitRepositoryUrl).NotEmpty();
+            RuleFor(x => x.JobType).IsInEnum();
+
+        }
+    }
+}

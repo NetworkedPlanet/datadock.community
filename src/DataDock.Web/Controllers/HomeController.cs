d46514409800029bf8ab45db5031807ef2d5bcc9
jennet <jen@networkedplanet.com>
2018-03-13 16:57:50 +0000

    Login from homepage

--- src/DataDock.Web/Controllers/HomeController.cs
+++ src/DataDock.Web/Controllers/HomeController.cs
@@ -2,16 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using DataDock.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DataDock.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = User.Identity;
+            var userViewModel = new UserViewModel();
+            await userViewModel.Populate(user as ClaimsIdentity);
+
+            return View(userViewModel);
         }
 
         public IActionResult Error()

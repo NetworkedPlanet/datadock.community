7213f1b4470c2c5fd3a42ce8b4dca481320d85ee
jennet <jen@networkedplanet.com>
2018-03-14 11:58:17 +0000

    Login

---
+++ src/DataDock.Web/Auth/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace DataDock.Web.Auth
+{
+    public class ApplicationUser : IPrincipal
+    {
+        public bool IsInRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IIdentity Identity { get; }
+    }
+}

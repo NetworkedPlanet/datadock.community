fc6a2e20d473d0d860303051e36799f9dbcd1133
jennet <jen@networkedplanet.com>
2018-03-20 14:37:32 +0000

    Dashboard Menu ViewComponents

---
+++ src/DataDock.Web/ViewComponents/DashboardMenuPrivateViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using DataDock.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DataDock.Web.ViewComponents
+{
+    [ViewComponent(Name = "DashboardMenuPrivate")]
+    public class DashboardMenuPrivateViewComponent : ViewComponent
+    {
+       
+        public DashboardMenuPrivateViewComponent()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string currentPage)
+        {
+            var dmvm = new DashboardMenuPrivateViewModel();
+            return View(dmvm);
+        }
+    }
+}
---
+++ src/DataDock.Web/ViewComponents/DashboardMenuPublicViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DataDock.Web.ViewComponents
+{
+    [ViewComponent(Name = "DashboardMenuPublic")]
+    public class DashboardMenuPublicViewComponent : ViewComponent
+    {
+        protected DashboardMenuPublicViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string currentPage)
+        {
+            return View();
+        }
+    }
+}

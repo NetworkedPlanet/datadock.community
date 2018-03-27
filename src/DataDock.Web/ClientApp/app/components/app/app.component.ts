5ff79d6fb90b0ee02349428c76e312d7ce90ad33
jennet <jen@networkedplanet.com>
2018-03-27 11:25:24 +0100

    print debug info in console

--- src/DataDock.Web/ClientApp/app/components/app/app.component.ts
+++ src/DataDock.Web/ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { AppService } from '../../shared/app.service';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+
+    error: string;
+
+    constructor(private elementRef: ElementRef, private appService: AppService){
+        this.error = '';
+    }
+
+    ngOnInit(): void {
+        this.printEnvironmentVariables();
+            // v0.1 set a dash view model and target repo simply to recreate the dashboard menu, this is unnecessary in v1.0 community edition
+    }
+
+    printEnvironmentVariables() {
+
+        // set in webpack config
+
+        if (IN_DEBUG) {
+
+            // let env = JSON.stringify(process.env);
+            // console.log(`process.env: ${env}`);
+
+            console.log(`IN_DEBUG: ${IN_DEBUG}`);
+            console.log(`VERSION: ${VERSION}`);
+            console.log(`API_URL: ${API_URL}`);
+        }
+    }
 }

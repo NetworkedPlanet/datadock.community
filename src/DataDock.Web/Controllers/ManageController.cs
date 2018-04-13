ca8f1a9a3a76124f43fc5dd7b886acd7a60ee2b7
jennet <jen@networkedplanet.com>
2018-04-13 16:09:18 +0100

    Seed a template via front end

--- src/DataDock.Web/Controllers/ManageController.cs
+++ src/DataDock.Web/Controllers/ManageController.cs
@@ -17,12 +17,14 @@ namespace DataDock.Web.Controllers
     public class ManageController : Controller
     {
         private readonly IDatasetStore _datasetStore;
+        private readonly ISchemaStore _schemaStore;
         private readonly IRepoSettingsStore _repoSettingsStore;
         private readonly IOwnerSettingsStore _ownerSettingsStore;
 
-        public ManageController(IDatasetStore datasetStore)
+        public ManageController(IDatasetStore datasetStore, ISchemaStore schemaStore)
         {
             _datasetStore = datasetStore;
+            _schemaStore = schemaStore;
         }
 
         public IActionResult Index()
@@ -41,6 +43,26 @@ namespace DataDock.Web.Controllers
                 foreach (var ds in datasets)
                 {
                     await _datasetStore.CreateOrUpdateDatasetRecordAsync(ds);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<IActionResult> SeedTemplates()
+        {
+            try
+            {
+                // create a few dummy datasets
+                var templates = GetDummyTemplates(1);
+                foreach (var t in templates)
+                {
+                    await _schemaStore.CreateOrUpdateSchemaRecordAsync(t);
                 }
 
                 return RedirectToAction("Index");
@@ -74,6 +96,25 @@ namespace DataDock.Web.Controllers
 
             datasets.Add(datasetInfo);
             return datasets;
+        }
+
+        private List<SchemaInfo> GetDummyTemplates(int num)
+        {
+            var schemas = new List<SchemaInfo>();
+
+            var csvwJson = new JObject(new JProperty("dc:title", "Test Dataset"), new JProperty("dcat:keyword", new JArray("one", "two", "three")));
+            var ownerId = User.Identity.Name;
+            var schemaInfo = new SchemaInfo()
+            {
+                OwnerId = ownerId,
+                RepositoryId = "repo-name",
+                SchemaId = Guid.NewGuid().ToString(),
+                LastModified = DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(1)),
+                Schema = csvwJson
+            };
+
+            schemas.Add(schemaInfo);
+            return schemas;
         }
     }
 }

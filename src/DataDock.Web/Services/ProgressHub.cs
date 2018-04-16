3bba87011bb5af2e79eacfde42388bb892cff827
jennet <jen@networkedplanet.com>
2018-04-16 17:10:24 +0100

    Include StatusUpdated method

--- src/DataDock.Web/Services/ProgressHub.cs
+++ src/DataDock.Web/Services/ProgressHub.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Datadock.Common.Models;
 using Microsoft.AspNetCore.SignalR;
 
 namespace DataDock.Web.Services
@@ -13,6 +14,12 @@ namespace DataDock.Web.Services
             // TODO: Change this to send to the specific user
             await Clients.All.SendAsync("progressUpdated", userId, jobId, progressMessage);
         }
-        
+
+        public async Task StatusUpdated(string userId, string jobId, JobStatus jobStatus)
+        {
+            // TODO: Change this to send to the specific user
+            await Clients.All.SendAsync("statusUpdated", userId, jobId, jobStatus);
+        }
+
     }
 }

e4e9ae892b1a80537c54d4b581761b7758d62cf0
Kal <kal@techquila.com>
2018-04-20 17:03:02 +0100

    Create a group per connected user and send user-specific messages to that
    Closes #42

--- src/DataDock.Web/Services/ProgressHub.cs
+++ src/DataDock.Web/Services/ProgressHub.cs
@@ -1,4 +1,5 @@
-﻿using Datadock.Common.Models;
+﻿using System;
+using Datadock.Common.Models;
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
@@ -9,19 +10,36 @@ namespace DataDock.Web.Services
         public async Task ProgressUpdated(string userId, string jobId, string progressMessage)
         {
             // TODO: Change this to send to the specific user
-            await Clients.All.SendAsync("progressUpdated", userId, jobId, progressMessage);
+            await Clients.Group(userId).SendAsync("progressUpdated", userId, jobId, progressMessage);
+            //await Clients.All.SendAsync("progressUpdated", userId, jobId, progressMessage);
         }
 
         public async Task StatusUpdated(string userId, string jobId, JobStatus jobStatus)
         {
             // TODO: Change this to send to the specific user
-            await Clients.All.SendAsync("statusUpdated", userId, jobId, jobStatus);
+            // await Clients.All.SendAsync("statusUpdated", userId, jobId, jobStatus);
+            await Clients.Group(userId).SendAsync("statusUpdated", userId, jobId, jobStatus);
         }
 
         public async Task SendMessage(string userId, string message)
         {
-            await Clients.All.SendAsync("sendMessage", userId, message);
+            //await Clients.All.SendAsync("sendMessage", userId, message);
+            await Clients.Group(userId).SendAsync("sendMessage", userId, message);
         }
 
+        public override async Task OnConnectedAsync()
+        {
+            try
+            {
+                var name = Context.User.Identity.Name;
+                await Groups.AddAsync(Context.ConnectionId, name);
+            }
+            catch (Exception)
+            {
+                // A connection from the worker role will not have a user identity
+            }
+
+            await base.OnConnectedAsync();
+        }
     }
 }

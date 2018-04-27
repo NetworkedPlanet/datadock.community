88bc429755ff781c01bad120bfeda8cc405c0c90
jennet <jen@networkedplanet.com>
2018-04-27 12:32:29 +0100

    Search Page

---
+++ src/DataDock.Web/ViewModels/SearchResultViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Datadock.Common.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataDock.Web.ViewModels
+{
+    public class SearchResultViewModel : BaseLayoutViewModel
+    {
+        public string SearchTag { get; private set; }
+
+        public IReadOnlyList<DatasetViewModel> Results { get; private set; }
+
+        public SearchResultViewModel()
+        {
+        }
+
+        public SearchResultViewModel(string tag, IEnumerable<DatasetInfo> searchResults)
+        {
+            SearchTag = tag;
+            Results = searchResults.Select(d => new DatasetViewModel(d)).ToList();
+        }
+    }
+}

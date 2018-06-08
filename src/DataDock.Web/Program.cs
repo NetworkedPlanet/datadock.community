37a139f848ab1efa034946a7911264a706cf1158
Jen Williams <digidatajen@gmail.com>
2018-06-08 13:02:55 +0100

    Feature datadock web config (#89)
    * Merge oauth settings with appsettings
    
    Closes #80
    
    * BUGFIX: Nullreference exception in TemplateLibrary
    
    * Make appSettings file(s) optional
    
    * BUGFIX: Null checks in TemplateViewModel

--- src/DataDock.Web/Program.cs
+++ src/DataDock.Web/Program.cs
@@ -21,12 +21,9 @@ namespace DataDock.Web
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((webHostBuilderContext, configurationbuilder) =>
                 {
-                    var environment = webHostBuilderContext.HostingEnvironment;
-                    var oauthSettingsFile = Path.Combine(environment.ContentRootPath, string.Format("oauth.{0}.json", environment.EnvironmentName.ToLower()));
-                    configurationbuilder
-                        .AddJsonFile("appSettings.json", optional: true)
-                        .AddJsonFile(oauthSettingsFile, optional: true);
-
+                    IHostingEnvironment env = webHostBuilderContext.HostingEnvironment;
+                    configurationbuilder.AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
                     configurationbuilder.AddEnvironmentVariables();
                 })
                 .UseStartup<Startup>()

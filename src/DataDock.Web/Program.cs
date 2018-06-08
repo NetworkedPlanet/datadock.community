1c0b6b7a44a89f09fb0cb32d71f0c66685945c4c
jennet <jen@networkedplanet.com>
2018-06-08 12:47:03 +0100

    Merge oauth settings with appsettings
    Closes #80

--- src/DataDock.Web/Program.cs
+++ src/DataDock.Web/Program.cs
@@ -21,12 +21,7 @@ namespace DataDock.Web
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((webHostBuilderContext, configurationbuilder) =>
                 {
-                    var environment = webHostBuilderContext.HostingEnvironment;
-                    var oauthSettingsFile = Path.Combine(environment.ContentRootPath, string.Format("oauth.{0}.json", environment.EnvironmentName.ToLower()));
-                    configurationbuilder
-                        .AddJsonFile("appSettings.json", optional: true)
-                        .AddJsonFile(oauthSettingsFile, optional: true);
-
+                    configurationbuilder.AddJsonFile("appSettings.json", optional: true);
                     configurationbuilder.AddEnvironmentVariables();
                 })
                 .UseStartup<Startup>()

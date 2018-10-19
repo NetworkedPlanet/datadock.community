6e876b4b8c2ccc131fbfc3568766be95985b853b
Kal Ahmed <techquila@gmail.com>
2018-10-19 15:04:36 +0100

    Legacy data import (#108)
    Work to support #54
    
    This basic importer can be built as a docker image that includes the console app and the data to be imported. It can be run via the docker command-line like so:
    
        docker run --rm -it --network container:dockercompose14270827642735103497_elasticsearch_1 datadock.import:latest /data/20181019_datasets.json /data/20181019_schemas.json http://localhost:9200
    
    NOTE: use the --network option to let the image use the same network stack as the elasticsearch container. This means it can then use the localhost address for the ES endpoint.

---
+++ src/DataDock.Import/Importer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using DataDock.Common.Elasticsearch;
+using DataDock.Common.Models;
+using DataDock.Import.Models;
+using Newtonsoft.Json;
+
+namespace DataDock.Import
+{
+    class Importer
+    {
+        private Options _options;
+        private DatasetStore _datasetStore;
+        private SchemaStore _schemaStore;
+
+        public Importer(Options opts, DatasetStore datasetStore, SchemaStore schemaStore)
+        {
+            _options = opts;
+            _datasetStore = datasetStore;
+            _schemaStore = schemaStore;
+        }
+
+        public async Task RunAsync()
+        {
+            await ImportDatasetsAsync();
+            await ImportSchemasAsync();
+        }
+
+        private async Task ImportDatasetsAsync()
+        {
+            var datasetJson = File.ReadAllText(_options.DatasetsJsonFile);
+            var datasets = JsonConvert.DeserializeObject<List<LegacyDatasetInfo>>(datasetJson);
+            foreach (var ds in datasets)
+            {
+                await _datasetStore.CreateOrUpdateDatasetRecordAsync(new DatasetInfo
+                {
+                    OwnerId = ds.OwnerId,
+                    RepositoryId = ds.RepositoryId,
+                    DatasetId = ds.DatasetId,
+                    LastModified = ds.LastModified,
+                    ShowOnHomePage = ds.ShowOnHomePage,
+                    CsvwMetadata = ds.Metadata,
+                    Tags = ds.Metadata["dcat:keyword"]?.ToObject<List<string>>()
+                });
+            }
+        }
+
+        private async Task ImportSchemasAsync()
+        {
+            var schemasJson = await File.ReadAllTextAsync(_options.SchemasJsonFile);
+            var schemas = JsonConvert.DeserializeObject<List<LegacySchemaInfo>>(schemasJson);
+            foreach (var s in schemas)
+            {
+                await _schemaStore.CreateOrUpdateSchemaRecordAsync(new SchemaInfo
+                {
+                    OwnerId = s.OwnerId,
+                    RepositoryId = s.RepositoryId,
+                    SchemaId = s.SchemaId,
+                    Schema = s.Schema
+                });
+            }
+        }
+    }
+}
